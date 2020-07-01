9619f8f5854c589ee5fe6483db8427904a8ea3d9
Craig <craig@packback.co>
2020-07-01 18:37:33 -0500

    Crank up the htmlproofer settings a little

--- ci.sh
+++ ci.sh
@@ -7,7 +7,8 @@ jekyll build
 # only_4xx:  Ignore all non-400s, because I get some timeouts when I run this locally
 # allow_hash_href:  Allow href="#"
 # enforce https:  Error if a non-https link is used
-OPTIONS="--check_html --enforce_https --only_4xx --allow_hash_href --url-ignore /twitter.com/"
+# url-ignore:  Twitter rate limits the CircleCI runners so the CI fails frequently
+OPTIONS="--check_html --enforce_https --only_4xx --allow_hash_href --url-ignore /twitter.com/ --check_img_http"
 if [ -z "$1" ]; then
     echo "Scanning entire site"
     htmlproofer ./_site $OPTIONS
