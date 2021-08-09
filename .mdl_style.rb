795f6a3cbef245850f38a63f00489e9325dc6a3b
Craig <craig@packback.co>
2021-08-08 23:59:15 -0500

    Rearranged mdl_style to document all the rules I knowingly ignore

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,7 +1,6 @@
 all
 
 exclude_rule 'MD032'
-exclude_rule 'MD033'
 exclude_rule 'MD034'
 exclude_rule 'MD036'
 exclude_rule 'MD046'
@@ -13,6 +12,10 @@ exclude_rule 'MD013'
 # MD026 Trailing punctuation in header
 # I write the occasional header that ends in ?
 exclude_rule 'MD026'
+
+# MD033 Inline HTML
+# Just use inline html sometimes
+exclude_rule 'MD033'
 
 # MD041 First line in file should be a top level header
 # Excluded because Jekyll files have a preamble
