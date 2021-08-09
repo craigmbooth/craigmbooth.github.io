b14aed5b692e7f403a4f5820ce4e3c2593f055a7
Craig <craig@packback.co>
2021-08-08 19:08:17 -0500

    Exclude MD041

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -17,3 +17,7 @@ exclude_rule 'MD033'
 exclude_rule 'MD034'
 exclude_rule 'MD036'
 exclude_rule 'MD046'
+
+# First line in file should be a top level header
+# Excluded because Jekyll files have a preamble
+exclude_rule 'MD041'
